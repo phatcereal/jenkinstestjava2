2e597b27b631b262b07b1e9c9299ef65e65cebc9
weed <weed@localhost>
2015-12-13 13:34:37 +0100

    weed

---
+++ AllTests.java
@@ -0,0 +1,11 @@
+package co.maximuspro.jenkinsjavatest2;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AddTest.class })
+public class AllTests {
+
+}
